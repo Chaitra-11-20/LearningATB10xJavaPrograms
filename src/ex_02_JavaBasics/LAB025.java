9cb16b4a1510143d23a014aaa6ce0cefaa33c192
chaitra <Chaitrachidanand2811@gmail.com>
2025-01-13 17:18:29 +0000

    13TH JAN 2025 -trial-2

--- src/ex_02_JavaBasics/LAB025.java
+++ src/ex_02_JavaBasics/LAB025.java
@@ -1,4 +1,9 @@
 package ex_02_JavaBasics;
 
 public class LAB025 {
+    public static void main(String[] args) {
+
+        int a=10;
+        System.out.printf("Value of number is %d",a);
+    }
 }
