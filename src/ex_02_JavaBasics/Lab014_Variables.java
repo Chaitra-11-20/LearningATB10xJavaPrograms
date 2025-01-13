117f2e7c296c9eebc832ef25334a897eb8f9b5e9
chaitra <Chaitrachidanand2811@gmail.com>
2025-01-13 17:02:25 +0000

    13TH JAN 2025

---
+++ src/ex_02_JavaBasics/Lab014_Variables.java
@@ -0,0 +1,13 @@
+package ex_02_JavaBasics;
+
+public class Lab014_Variables {
+    public static void main(String[] args) {
+        System.out.println("Hello!!");
+
+
+    }
+
+    public void main(String args) {
+
+    }
+}
---
+++ src/ex_02_JavaBasics/Lab019_Interview_QnA.java
@@ -0,0 +1,11 @@
+package ex_02_JavaBasics;
+
+public class Lab019_Interview_QnA {
+    public static void main(String[] args) {
+        int _2 = 123;
+        int _1 = 123;
+        int _0 = 123;
+
+        //It is a valid code
+    }
+}
