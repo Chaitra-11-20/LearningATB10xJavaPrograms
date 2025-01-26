e3700e2f225e3de2ba9e084d1abb2231cba949ac
chaitra <Chaitrachidanand2811@gmail.com>
2025-01-26 15:24:02 +0000

    26th jan

---
+++ src/ex_14_Strings/Lab133.java
@@ -0,0 +1,19 @@
+package ex_14_Strings;
+
+public class Lab133 {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+       s1= s1.concat("World"); //
+        System.out.println(s1);
+
+
+
+        String c1="Chai";
+       c1= c1.concat("Chaiaaa");
+        System.out.println(c1);
+
+        String c2="tree";
+        System.out.println(c2.concat("ttt"));
+       // System.out.println(c2);
+    }
+}
